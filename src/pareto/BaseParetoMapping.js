b7ef36cedcde37a2afe26a74f5ecdee340e8648f
Anton Chengaev <anton.chengaev@anychart.com>
2020-08-21 12:11:42 +0800

    ref

--- src/pareto/BaseParetoMapping.js
+++ src/pareto/BaseParetoMapping.js
@@ -5,16 +5,17 @@ goog.require('anychart.data.Mapping');
 
 /**
  * Base pareto mapping class.
+ *
+ * Set mapping for instance of class based on passed parent view.
+ *
  * @extends {anychart.data.Mapping}
  * @param {!anychart.data.IView} parentView Parent view. The last view is a mapping.
  * @constructor
  */
 anychart.paretoModule.BaseParetoMapping = function(parentView) {
     var mappings = parentView.getMappings();
-    var mapping;
-    if (mappings.length) {
-        mapping = mappings[0].getMapping();
-    }
+    var mapping = mappings.length ? mappings[0].getMapping() : void 0;
+
     anychart.paretoModule.BaseParetoMapping.base(this, 'constructor', parentView, mapping);
 };
 goog.inherits(anychart.paretoModule.BaseParetoMapping, anychart.data.Mapping);
