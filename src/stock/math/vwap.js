603bf5848556b8e68edfd7e52294adcd60109e6e
Alexander Shestakov <shestacov92@gmail.com>
2021-04-01 12:25:06 +0800

    update indicator.Base to provide chart instance in the indicator context and implement indicator calculatation according to the chart data grouping unit

--- src/stock/math/vwap.js
+++ src/stock/math/vwap.js
@@ -7,6 +7,8 @@ goog.require('anychart.utils');
  *   cumulativeVolume: number,
  *   cumulativeTransactionsValue: number,
  *   beginOfDay: number,
+ *   chart: anychart.core.Chart,
+ *   allowedUnits: Array.<string>,
  *   dispose: Function
  * }}
  */
@@ -15,13 +17,18 @@ anychart.stockModule.math.vwap.Context;
 
 /**
  * Creates context for VWAP indicator calculation.
+ * @param {anychart.core.Chart} chart
  * @return {anychart.stockModule.math.vwap.Context}
  */
-anychart.stockModule.math.vwap.initContext = function() {
+anychart.stockModule.math.vwap.initContext = function(chart) {
   return {
     cumulativeVolume: 0,
     cumulativeTransactionsValue: 0,
     beginOfDay: NaN,
+    // chart instance to get access to the chart data grouping
+    chart: chart,
+    // vwap can be rendered on the following data grouping units
+    allowedUnits: ['hour', 'millisecond', 'minute', 'second'],
     /**
      * @this {anychart.stockModule.math.vwap.Context}
      */
@@ -72,34 +79,42 @@ anychart.stockModule.math.vwap.calculate = function(context, high, low, close, v
  * @this {anychart.stockModule.math.vwap.Context}
  */
 anychart.stockModule.math.vwap.calculationFunction = function(row, context) {
-  // TODO: разобраться с группировкой и описать все в api
-  var currentTimestamp = row.getX();
+  var currentTimestamp = anychart.utils.toNumber(row.getX());
   if (isNaN(context.beginOfDay) || currentTimestamp - context.beginOfDay >= 1000 * 60 * 60 * 24) {
     var curDateObj = new Date(currentTimestamp);
     var curYear = curDateObj.getUTCFullYear();
     var curMonth = curDateObj.getUTCMonth();
     var curDay = curDateObj.getUTCDate();
     context.beginOfDay = Date.UTC(curYear, curMonth, curDay, 0, 0, 0);
-    context.dispose();
+    context['dispose']();
   }
-  // надо поискать как можно определить здесь текущую группировку
-  var high = anychart.utils.toNumber(row.get('high'));
-  var low = anychart.utils.toNumber(row.get('low'));
-  var close = anychart.utils.toNumber(row.get('close'));
-  var volume = anychart.utils.toNumber(row.get('volume'));
-  var result = anychart.stockModule.math.vwap.calculate(context, high, low, close, volume);
-  row.set('vwapValue', result);
+  // get current data grouping unit
+  var unit = context.chart.grouping().getCurrentDataInterval()['unit'];
+
+  if (context.allowedUnits.indexOf(unit) === -1) {
+    // do not calculate indicator on disallowed data grouping units
+    row.set('vwapValue', NaN);
+  } else {
+    // calculate indicator on allowed data grouping units
+      var high = anychart.utils.toNumber(row.get('high'));
+      var low = anychart.utils.toNumber(row.get('low'));
+      var close = anychart.utils.toNumber(row.get('close'));
+      var volume = anychart.utils.toNumber(row.get('volume'));
+      var result = anychart.stockModule.math.vwap.calculate(context, high, low, close, volume);
+      row.set('vwapValue', result);
+  }
 };
 
 
 /**
  * Creates VWAP computer for the given table mapping.
  * @param {anychart.stockModule.data.TableMapping} mapping
+ * @param {anychart.core.Chart} chart
  * @return {anychart.stockModule.data.TableComputer}
  */
-anychart.stockModule.math.vwap.createComputer = function(mapping) {
+anychart.stockModule.math.vwap.createComputer = function(mapping, chart) {
   var result = mapping.getTable().createComputer(mapping);
-  result.setContext(anychart.stockModule.math.vwap.initContext());
+  result.setContext(anychart.stockModule.math.vwap.initContext(chart));
   result.setStartFunction(anychart.stockModule.math.vwap.startFunction);
   result.setCalculationFunction(anychart.stockModule.math.vwap.calculationFunction);
   result.addOutputField('vwapValue');
